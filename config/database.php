6907c2adebbd39116be82d378db81ae1862d0a99
MagedSarhan <ma20.sa22.de@gmail.com>
2026-05-04 17:09:53 +0300

    update

--- config/database.php
+++ config/database.php
@@ -3,8 +3,16 @@
  * علامة | ALAMAH — Database Configuration
  */
 
+// define('DB_HOST', 'localhost');
+// define('DB_NAME', 'alamah_db');
+// define('DB_USER', 'root');
+// define('DB_PASS', '');
+// define('DB_CHARSET', 'utf8mb4');
+
+
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'alamah_db');
-define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_NAME', 'u198070152_alamah');
+define('DB_USER', 'u198070152_alamah');
+define('DB_PASS', '(Alamah)1');
 define('DB_CHARSET', 'utf8mb4');
+
